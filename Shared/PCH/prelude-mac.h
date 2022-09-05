fcc0eace6f1405a3b98fa4b6e6cbd9bb61face4c
Ian Gregory <justcheesy@outlook.com>
2022-09-04 20:28:57 -0400

    #define vfork fork

--- Shared/PCH/prelude-mac.h
+++ Shared/PCH/prelude-mac.h
@@ -7,7 +7,9 @@
 #import <Carbon/Carbon.h>
 #import <CommonCrypto/CommonDigest.h>
 #import <CoreFoundation/CoreFoundation.h>
+#import <CoreGraphics/CoreGraphics.h>
 #import <CoreServices/CoreServices.h>
+#import <CoreText/CoreText.h>
 #import <Security/Security.h>
 #import <ServiceManagement/ServiceManagement.h>
 #import <SystemConfiguration/SystemConfiguration.h>
@@ -17,5 +19,10 @@
 
 #import <os/activity.h>
 #import <os/log.h>
+
+// vfork() is deprecated in macOS 12+, with posix_spawn() and fork() as intended replacements.
+// However, it's still useful to indicate where vfork() can be used on non-macOS systems,
+// so rather than s/vfork/fork/g in the actual source, let the preprocessor do it.
+#define vfork fork
 
 #endif /* end of include guard: PRELUDE_MAC_H_X1SR1JB2 */
